5cbad9ccf833afd21015af3dda308a6d988fc4c5
WhisperKnight <61612853+Bryson-C@users.noreply.github.com>
2022-08-04 11:53:15 -0600

    Add files via upload
    Added Functionality To Exit Compilation On Errors Occuring (Because I Would Just Do Anything Possible To Recover From Errors, So Now Its Optional)
    (I May Or May Not Have Included This File Originally, For That I Am Sorry)

--- CLL.hpp
+++ CLL.hpp
@@ -9,6 +9,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <assert.h>
 
 namespace CLL_StdLabels {
     constexpr const char* Data = "Data";
@@ -21,15 +22,17 @@ namespace CLL_StdLabels {
     constexpr const char* OffendingVariable = "Offending Variable";
 
 };
-
-inline void CLL_StdErr(std::string message, std::initializer_list<std::string> labels, std::initializer_list<std::string> labelText) {
+#define PANIC_ON_ERROR true
+inline void CLL_StdErr(std::string message, std::initializer_list<std::string> labels = {}, std::initializer_list<std::string> labelText = {}) {
     std::cerr << message << "\n";
     for (uint32_t i = 0; i < labels.size(); i++) {
         std::cerr << "  " << labels.begin()[i] << ": " << labelText.begin()[i] << "\n";
     }
     std::cout << "\n";
+    if (PANIC_ON_ERROR)
+        exit(-1);
 }
-inline void CLL_StdOut(std::string message, std::initializer_list<std::string> labels, std::initializer_list<std::string> labelText) {
+inline void CLL_StdOut(std::string message, std::initializer_list<std::string> labels = {}, std::initializer_list<std::string> labelText = {}) {
     std::cout << message << "\n";
     for (uint32_t i = 0; i < labels.size(); i++) {
         std::cout << "  " << labels.begin()[i] << ": " << labelText.begin()[i] << "\n";
